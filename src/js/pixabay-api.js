f819df6258d6720813637fd150621ee621192bd9
Denis Bochev <denisbo4ev@gmail.com>
2024-10-27 11:41:21 +0200

    add-gallery

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,25 +1,15 @@
-import axios from 'axios';
-
 const API_KEY = '46607456-5552cc86243543e4de6027df0';
 const BASE_URL = 'https://pixabay.com/api/';
 
 // Функція для запиту з Pixabay
-async function fetchImages(query, page = 1) {
-  try {
-    const response = await axios.get(BASE_URL, {
-      params: {
-        q: query,
-        page: page,
-        key: API_KEY,
-        image_type: 'photo',
-        per_page: 12
-      }
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    throw error;  // Рекомендується кидати помилку для обробки на рівні виклику функції
-  }
-}
+export async function fetchImages(query, page = 1, perPage = 12) {
+  const response = await fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
 
-export default fetchImages;
+  if (!response.ok) {
+    throw new Error('Error fetching data');
+  }
+
+  return response.json();
+}
